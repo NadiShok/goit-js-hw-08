5b3f3f03dad3498e0e49c60f37acd77f9671ca74
NadiShok <nadinsh85@gmail.com>
2023-12-08 20:16:45 +0200

    commit

--- js/task-03.js
+++
@@ -1,7 +0,0 @@
-const textInput = document.querySelector("#name-input");
-const output = document.querySelector("#name-output");
-
-textInput.addEventListener("input", (event) => {
-    const inputValue = event.target.value.trim()
-    output.textContent = inputValue ? inputValue : "Anonymous";
-});
